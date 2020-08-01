7e3e16bf9a3c5add66424abad0074f6a0488fbb3
cky <2907183182@qq.com>
2020-08-01 21:53:06 +0800

    2020/8/1

--- microService/provider/src/main/java/com/cky/microService/controller/ProviderController.java
+++ microService/provider/src/main/java/com/cky/microService/controller/ProviderController.java
@@ -1,21 +1,19 @@
 package com.cky.microService.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/provide/")
+@Slf4j
 public class ProviderController {
-    private static final Logger LOG = LoggerFactory.getLogger(ProviderController.class);
 
-    @Value("${server.port}")
-    private String Port;
-
-//    @GetMapping
-//    public String service(){
-//        LOG.info("provider invoke");
-//        return "provider invoke";
-//    }
+    @GetMapping("/sayHello/{name}")
+    public String sayHello(@PathVariable("name") String name){
+        log.info("I'm calling nacos-provider service by dynamic gateway...");
+        return name + " Hi~, I'm from nacos-provider";
+    }
 }
