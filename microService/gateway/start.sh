a619a28cbf1a3f5a02e7d9eac5f5bd6770bcf92c
cky <2907183182@qq.com>
2020-08-06 21:05:33 +0800

    2020/8/6

--- microService/gateway/start.sh
+++ microService/gateway/start.sh
@@ -1,3 +1,3 @@
 #！/bin/bash
-nohup java -javaagent:/apps/agent/skywalking-agent.jar -Dskywalking.agent.service_name=gateway -Dskywalking.collector.backend_service=101.132.178.134:11800 -jar /apps/gateway.jar &
+nohup java -javaagent:/apps/agent/skywalking-agent.jar -Dskywalking.agent.service_name=provider -Dskywalking.collector.backend_service=101.132.178.134:11800 -jar /apps/provider.jar &
 
